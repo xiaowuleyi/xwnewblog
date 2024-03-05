c8430b071f6333d56c5f788d036291d80853f8a3
小吴乐意 <102008938+xiaowuleyi@users.noreply.github.com>
2024-03-05 16:16:58 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,9 +17,9 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '小吴乐意', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '随心所欲活着的人', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.xiaowuleyi.com', // 网站地址
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '小吴乐意的博客', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || '随心所欲活着的人，生活一直在路上', // 作者简介
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.xiaowuleyi.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '小吴, 博客, 小吴乐意博客, 生活记录, 菲律宾', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
